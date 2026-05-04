c9b0f452036839b70047bdaeb7fc72e57b9a73a5
duonghb53 <duonghb53@gmail.com>
2026-05-05 00:13:53 +0700

    feat: update baseUrl in config and add GitHub Pages deployment workflow

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "duonghb53.github.io/quartz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "modified",
     theme: {
